dd4c19e0ae17abca02876447dcf3469758e2854e
sunny yuen <yuens1002@gmail.com>
2017-01-13 12:52:01 -0800

    responsive layout with media queries

--- script/main.js
+++ script/main.js
@@ -18,7 +18,7 @@ var slideshow = {
 			text: 'Praesent malesuada orci in arcu faucibus commodo. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla et odio.'
 		},
 		{
-			title: 'USB Dulplication',
+			title: 'Additional Services',
 			image: 'http://lorempixel.com/600/300/technics/',
 			text: 'Maecenas condimentum non magna vitae tristique. Quisque sed faucibus est, a finibus dui. Praesent vehicula tellus tortor, non vehicula urna.'
 		}
@@ -26,17 +26,29 @@ var slideshow = {
 	showSlide: function() {
 		document.getElementById('carousel').querySelector('img')
 			.setAttribute('src', this.slides[0].image);
-		document.getElementById('carousel').querySelector('div')
+		document.getElementById('carousel').querySelector('.text')
 			.innerHTML = '<h4>'+ this.slides[0].title + '</h4>' + '<p>' + this.slides[0].text + '</p>';
 	},
-	getNext: function() {
-		(this.currentSlide === this.slides.length) && (this.currentSlide = 0);
-		var next = this.currentSlide++;
+	nextSlide: function() {
+		(this.currentSlide === this.slides.length-1) && (this.currentSlide = -1);
+		this.currentSlide++;
+		var next = this.currentSlide;
 		console.log(next);
 		document.querySelector('img')
 			.setAttribute('src', this.slides[next].image);
-		document.getElementById('carousel').querySelector('div')
+		document.getElementById('carousel').querySelector('.text')
 			.innerHTML = '<h4>'+ this.slides[next].title + '</h4>' + '<p>' + this.slides[next].text + '</p>';
+
+	},
+	prevSlide: function() {
+		(this.currentSlide < 1) && (this.currentSlide = this.slides.length);
+		this.currentSlide--;
+		var prev = this.currentSlide;
+		console.log(prev);
+		document.querySelector('img')
+			.setAttribute('src', this.slides[prev].image);
+		document.getElementById('carousel').querySelector('.text')
+			.innerHTML = '<h4>'+ this.slides[prev].title + '</h4>' + '<p>' + this.slides[prev].text + '</p>';
 	}
 };
 
